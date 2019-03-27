e0e2db677ac8dd03750ae2995a7b37745026fd66
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-27 21:07:27 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,10 +21,10 @@ ORDER BY users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS \"over\" FROM projects
 JOIN pledges on projects.id = pledges.project_id
 GROUP BY projects.id
-HAVING (SUM(pledges.amount) - projects.funding_goal) >= 0;"
+HAVING over >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
